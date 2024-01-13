69ae3f65075e42f4b8314fa637d5b98904a5d7e8
ahonkazi <aohinuzzaman420@gmail.com>
2024-01-13 14:27:22 +0600

    chat footer input done

--- src/app/(withauth)/t/[unique_id]/_components/ChatFooter.jsx
+++ src/app/(withauth)/t/[unique_id]/_components/ChatFooter.jsx
@@ -4,34 +4,36 @@ import { IoAdd } from "react-icons/io5";
 import { LuSendHorizonal } from 'react-icons/lu';
 
 const ChatFooter = () => {
-    const [inputText, setInputText] = useState('');
-    const [formatedText, setFormatedText] = useState('');
-    const chatInputRef = useRef(null);
-    const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+    const messageFieldRef = useRef();
+    const [placeHolderStatus, setPlaceholderStatus] = useState(true);
+    const handleSubmit = () => {
+        event.preventDefault();
+        console.log(formatEmoji(messageFieldRef.current.innerHTML))
+    }
+    const handleKeyUp = (event) => {
+        if (event.shiftKey && event.key === "Enter") {
+            handleSubmit(); // Prevent default line break behavior
 
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter" && event.shiftKey) {
-            event.preventDefault();
-            setInputText((prevText) => prevText + "\n");
-        } else if (event.key === "Enter") {
-            event.preventDefault();
-            setInputText((prevText) => prevText + "<br>");
         }
-    };
+    }
+    const handleInput = (e) => {
+        if (e.target.innerText == "\n") {
+            setPlaceholderStatus(true)
+        } else {
+            setPlaceholderStatus(false)
+        }
+    }
+    const handleFocus = () => {
+        console.log('focused');
 
-    const handleInputChange = (event) => {
-        const wrappedTextInput = event.target.textContent;
-        const wrappedText = wrappedTextInput.replace(emojiRegex, '<span class="emoji">$1</span>');
-        setInputText(wrappedTextInput);
-        setFormatedText(wrappedText);
-    };
-
-    useEffect(() => {
-        console.log(formatedText);
-    }, [formatedText]);
-
+    }
+    const formatEmoji = (text) => {
+        const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+        const wrappedText = text.replace(emojiRegex, '<span class="emoji">$1</span>')
+        return wrappedText;
+    }
     return (
-        <div className='shrink-0 h-auto  bg-base-200 container__area flex items-center gap-x-4'>
+        <form onSubmit={(e) => { e.preventDefault(); handleSubmit() }} className='shrink-0 h-auto  bg-base-200 container__area flex items-center gap-x-4'>
             <div className="left__controll">
                 <button className='shrink-0'>
                     <label className="btn min-h-0 btn-icon shrink-0 !p-0 text-2xl bg-base-100 shadow-lg  btn-circle swap swap-rotate">
@@ -43,22 +45,25 @@ const ChatFooter = () => {
                 </button>
             </div>
             <div className="input-field rounded-md w-full max-h-[80px] overflow-y-auto">
-                <div className="input rounded-md w-full h-auto py-3 border flex items-center bg-base-100 border-base-300">
+                <div onFocus={(e) => handleFocus(e)} role='textbox' className={`input ${placeHolderStatus && ('placeholder')} rounded-md w-full h-auto py-3 border flex items-center bg-base-100 border-base-300`}>
                     <div
-                        ref={chatInputRef}
+                        ref={messageFieldRef}
                         contentEditable
+                        onInput={handleInput}
+                        onKeyDown={handleKeyUp}
                         suppressContentEditableWarning
-                        className="w-full h-full bg-transparent border-none outline-none"
-                        onInput={(e) => handleInputChange(e)}
-                        onKeyDown={(e) => handleKeyDown(e)} // Attach to onKeyDown
-                    />
+                        className={`w-full h-full bg-transparent border-none outline-none`}
+                        role='textbox'
+                    >
+                    </div>
+
                 </div>
             </div>
 
-            <button className='text-2xl btn hover:bg-transparent px-0 hover:border-transparent shrink-0'>
+            <button type='submit' className='text-2xl btn hover:bg-transparent px-0 hover:border-transparent shrink-0'>
                 <LuSendHorizonal />
             </button>
-        </div >
+        </form >
     )
 }
 
