292dd5b5589a6f903d96b731a8b963789521a6df
ahonkazi <aohinuzzaman420@gmail.com>
2024-01-14 14:31:32 +0600

    chat list updated

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -5,7 +5,7 @@ import ConversationList from './ConversationList';
 
 const ChatList = () => {
     return (
-        <aside className='flex w-[440px] shrink-0 flex-col h-screen '>
+        <aside className='flex w-[420px] shrink-0 flex-col h-screen '>
             <ChatListHeader />
             <ConversationList />
         </aside>
